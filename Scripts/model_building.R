7c30a1e3c0a2759b2b96ec88bfc0b5709b115266
Sahil Sharma <sahilsharmahimalaya@gmail.com>
2024-12-23 02:57:59 +0530

    n gram counting function created.

--- Scripts/model_building.R
+++ Scripts/model_building.R
@@ -3,3 +3,37 @@
 ################# Building N gram Model ##############################################
 ######################################################################################
 
+#################################
+##### Count N-grams #############
+#################################
+
+
+count_n_grams <- function(tokens_data, n = 2) {
+        
+        #Add Start of Sentence and End of Sentence Tokens
+        start_token <- 
+        #function
+        add_s <- function(doc){
+                c(replicate(n-1, "<s>"), doc, "</s>")
+        }
+        
+        #update_tokens
+        updated_tokens <- lapply(tokens_data, add_s)
+        updated_tokens <- tokens(updated_tokens)
+        
+        #Generate Tokens
+        tokens <- tokens_ngrams(updated_tokens, n = n)
+        
+        #Tokens Frequency
+        freq <- textstat_frequency(dfm(tokens)) %>% select(feature, frequency)
+        
+        #Print Message
+        print("Task Complete")
+        
+        return(list(freq, updated_tokens, tokens))
+}
+
+#apply 
+sample <- count_n_grams(test$tokens_unk, n = 3)
+
+
