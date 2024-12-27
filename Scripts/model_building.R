607767bbcb412e27a3deb815ab695000489af2a3
sahil-datascience <sahilsharmahimalaya@gmail.com>
2024-12-27 09:31:23 +0530

    Archived Functions, Not suitable, Slow for current project

--- Scripts/model_building.R
+++ Scripts/model_building.R
@@ -3,40 +3,8 @@
 ################# Building N gram Model ##############################################
 ######################################################################################
 
-#################################
-##### Count N-grams #############
-#################################
 
-
-count_n_grams <- function(tokens_data, n = 2) {
-        
-        #Add Start of Sentence and End of Sentence Tokens
-        start_token <- 
-        #function
-        add_s <- function(doc){
-                c(replicate(n-1, "<s>"), doc, "</s>")
-        }
-        
-        #update_tokens
-        updated_tokens <- lapply(tokens_data, add_s)
-        updated_tokens <- tokens(updated_tokens)
-        
-        #Generate Tokens
-        tokens <- tokens_ngrams(updated_tokens, n = n)
-        
-        #Tokens Frequency
-        freq <- textstat_frequency(dfm(tokens)) %>% select(feature, frequency)
-        
-        #Print Message
-        print("Task Complete")
-        
-        return(freq)
-}
-
-#apply 
-sample <- count_n_grams(test$tokens_unk, n = 3)
-head(sample)
-
+#list_ngram_counts <- list(unigrams, bigrams, trigrams, fourgrams)
 
 #####################################################
 ######### Probability Estimator #####################
@@ -55,7 +23,8 @@ estimate_probability <- function(word, previous_n_gram, n_gram_counts, n_plus1_g
         }
         
         #get freq count
-        freq_previous_n_gram <- feature_lookup(ngram = word, counts = n_gram_counts)
+        freq_previous_n_gram <- feature_lookup(ngram = previous_n_gram,
+                                               counts = n_gram_counts)
         
         #calculate denominator
         denominator <- freq_previous_n_gram + k * vocabulary_size
@@ -79,40 +48,86 @@ estimate_probability <- function(word, previous_n_gram, n_gram_counts, n_plus1_g
 }
 
 #apply function
-n_gram_counts = count_n_grams(test$tokens_unk, n = 1)
-n_plus1_gram_counts = count_n_grams(test$tokens_unk, n = 2)
-word <- "view"
-previous_n_gram <- "sea"
 
-estimate_probability(word, "window", n_gram_counts, n_plus1_gram_counts,
+word <- "cat"
+previous_n_gram <- "a"
+
+estimate_probability(word, previous_n_gram, unigrams, bigrams,
                      vocabulary_size = length(vocabulary),
                      k = 1.0)
 
+#Sample Exercise
+sentences = rbind(c("i like a cat"),
+                   c("this dog is like a cat"))
+
+colnames(sentences) <- "Text"
+rownames(sentences) <- c("1", "2")
+
+sentences
+
+sentences <- pre_process(sentences, coverage_threshold = 100)
+
+unique_words <- sentences$vocabulary
+
+sentences <- sentences$tokens_unk
+
+unigram_counts = count_n_grams(sentences, 1)
+bigram_counts = count_n_grams(sentences, 2)
+
+estimate_probability("cat", "a",
+                                unigram_counts,
+                                bigram_counts,
+                                length(unique_words), k=1)
 
 ##########################################################################################
 ################## Estimate Probabilities for All Words ##################################
 ##########################################################################################
 
-count_all_prob <- function(previous_n_gram, n_gram_counts, n_plus1_gram_counts,
+estimate_probabilities <- function(previous_n_gram, n_gram_counts, n_plus1_gram_counts,
                            vocabulary, k = 1.0){
         
         #Add end-of-sentence and unk tokens to vocab
-        up_vocab <- tibble(vocabulary) %>% rbind(c("</s>"), c("<unk>"))
+        up_vocab <- tibble(vocabulary) %>% rbind(c("</s>"), c("unk"))
         
         #Vocab size
         vocabulary_size <- length(up_vocab)
         
+        # Initialize an empty data frame to store the results
+        results_df <- data.frame(word = character(), prob = numeric())
         #Estimate Probability for all words 
-        for (word in up_vocab$vocabulary){
-                probability = estimate_probability(word, previous_n_gram,
-                                                   n_gram_counts, n_plus1_gram_counts,
+        for (i in seq(nrow(up_vocab))){
+                
+                word <- up_vocab$vocabulary[i]
+                
+                #estimate the prob for next word
+                #from the vocabuluary
+                prob <- estimate_probability(word, previous_n_gram,
+                                                   n_gram_counts,
+                                                   n_plus1_gram_counts,
                                                    vocabulary_size, k=k)
-        up_vocab_prob <- up_vocab %>% mutate(prob = probability)
+                
+                print(i)
+                
+        #attach prob score to each word in vocab
+       
+        # Add the word and its probability to the results data frame
+        results_df <- rbind(results_df, data.frame(word = word, prob = prob))
         
-        return(up_vocab_prob)}
+        } 
+        
+return(results_df)
+        
 }
 
 #apply
-count_all_prob("my", n_gram_counts, n_plus1_gram_counts,
+estimate_probabilities("case_of", bigrams, trigrams,
                vocabulary, k = 1.0) %>% arrange(-prob)
+
+
+#Sample Exercise
+
+#note: the data for this exercise is dependent on the above exercise below the 
+# estimate probability function
+
+estimate_probabilities("a", unigram_counts, bigram_counts, unique_words, k=1)
 
